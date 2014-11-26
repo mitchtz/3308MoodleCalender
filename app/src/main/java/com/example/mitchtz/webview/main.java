0057b7b0aaf912db0a37e7b4dd943d2ef5c88010
mitchtz <mitchtz@gmail.com>
2014-11-26 13:03:13 -0700

    Added all files from project

---
+++ app/src/main/java/com/example/mitchtz/webview/main.java
@@ -0,0 +1,86 @@
+package com.example.mitchtz.webview;
+
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.webkit.WebViewClient;
+
+import java.io.File;
+
+
+public class main extends Activity {
+    WebView ourBrow;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        final DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+
+        final File destinationDir = new File(Environment.getExternalStorageDirectory(), getPackageName());
+        if (!destinationDir.exists()) {
+            destinationDir.mkdir(); // Don't forget to make the directory if it's not there
+        }
+
+
+        // Use a custom layout file
+        setContentView(R.layout.activity_main);
+        ourBrow = (WebView) findViewById(R.id.webView1);
+        ourBrow.getSettings().setJavaScriptEnabled(true);
+        ourBrow.setInitialScale(50);
+        ourBrow.getSettings().setUseWideViewPort(true);
+        ourBrow.setVerticalScrollBarEnabled(false);
+        ourBrow.setHorizontalScrollBarEnabled(false);
+        ourBrow.loadUrl("https://moodle.cs.colorado.edu/calendar/export.php");
+
+        ourBrow.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode,
+                                        String description, String failingUrl) {
+                Log.d("WEB_VIEW_TEST", "error code:" + errorCode + " - " + description);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // handle different requests for different type of files
+                // this example handles downloads requests for .apk and .mp3 files
+                // everything else the webview can handle normally
+                if (url.endsWith(".ical")) {
+                    Uri source = Uri.parse(url);
+                    // Make a new request pointing to the .apk url
+                    DownloadManager.Request request = new DownloadManager.Request(source);
+                    // appears the same in Notification bar while downloading
+                    request.setDescription("Downloading Calender");
+                    request.setTitle("calender.ical");
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                        request.allowScanningByMediaScanner();
+                        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                    }
+                    // save the file in the "Downloads" folder of SDCARD
+                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "SmartPigs.apk");
+                    // get download service and enqueue file
+                    DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                    manager.enqueue(request);
+                }
+                // if there is a link to anything else than .apk or .mp3 load the URL in the webview
+                else view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+}
